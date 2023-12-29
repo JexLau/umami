c0a50e4b1f4030087733e31d909456e82064dc55
JexLau <jexlau.dev@gmail.com>
2023-12-30 03:40:35 +0800

    fix: add log

--- scripts/check-db.js
+++ scripts/check-db.js
@@ -34,7 +34,7 @@ async function checkEnv() {
   if (!process.env.DATABASE_URL) {
     throw new Error('DATABASE_URL is not defined.');
   } else {
-    success('DATABASE_URL is defined.');
+    success('DATABASE_URL is defined.', process.env.DATABASE_URL);
   }
 }
 
